a15e8b6360db3740390a2d345774a9a89061bbce
muozez <muhammetylmaz128@gmail.com>
2026-01-22 16:25:20 +0300

    added timestamp, TTL/expire handler

--- server.py
+++ server.py
@@ -2,51 +2,66 @@ import socket
 import threading
 import signal
 import sys
+import time
 
 HOST = "127.0.0.1"
 PORT = 1717
 
 store = {
-    "name": "muozez",
-    "status": "active"
+    "name": ("muozez", None),
+    "status": ("active", None)
 }
 
 lock = threading.Lock()
 server_socket = None
 running = True
 
+def is_expired(expire_at):
+    return expire_at is not None and time.time() > expire_at
+
 def handle_client(conn, addr):
     print(f"Connected: {addr}")
     with conn:
         while running:
-            try:
-                data = conn.recv(1024)
-                if not data:
-                    break
-
-                message = data.decode().strip()
-                print(f"{addr} -> {message}")
-
-                parts = message.split(" ", 2)
-                command = parts[0].upper()
-
-                if command == "GET" and len(parts) == 2:
-                    key = parts[1]
-                    with lock:
-                        value = store.get(key, "KEY_NOT_FOUND")
-                    conn.sendall((value + "\r\n").encode())
-
-                elif command == "SET" and len(parts) == 3:
-                    key, value = parts[1], parts[2]
-                    with lock:
-                        store[key] = value
-                    conn.sendall(b"200 OK\r\n")
-
-                else:
-                    conn.sendall(b"INVALID_COMMAND\r\n")
-
-            except ConnectionResetError:
+            data = conn.recv(1024)
+            if not data:
                 break
+
+            message = data.decode().strip()
+            print(f"{addr} -> {message}")
+
+            parts = message.split(" ")
+            command = parts[0].upper()
+
+            if command == "GET" and len(parts) == 2:
+                key = parts[1]
+                with lock:
+                    item = store.get(key)
+                    if not item:
+                        conn.sendall(b"KEY_NOT_FOUND\r\n")
+                        continue
+
+                    value, expire_at = item
+                    if is_expired(expire_at):
+                        del store[key]
+                        conn.sendall(b"KEY_EXPIRED\r\n")
+                        continue
+
+                conn.sendall((value + "\r\n").encode())
+
+            elif command == "SET" and len(parts) in (3, 4):
+                key = parts[1]
+                value = parts[2]
+                ttl = int(parts[3]) if len(parts) == 4 else None
+                expire_at = time.time() + ttl if ttl else None
+
+                with lock:
+                    store[key] = (value, expire_at)
+
+                conn.sendall(b"200 OK\r\n")
+
+            else:
+                conn.sendall(b"INVALID_COMMAND\r\n")
 
     print(f"Disconnected: {addr}")
 
