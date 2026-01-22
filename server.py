28024d298e9d55e4ba11c2ad1e8b8e54ec1c51d4
muozez <muhammetylmaz128@gmail.com>
2026-01-22 16:07:36 +0300

    The code would be spaghetti. I encapsulated it under function.

--- server.py
+++ server.py
@@ -3,25 +3,24 @@ import socket
 HOST = "127.0.0.1"
 PORT = 1717
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("Server listening...")
-    conn, addr = s.accept()
+store = {
+    "name": "muozez",
+    "age": 22
+}
 
+
+def handle_client(conn, addr):
+    print(f"COnnected {addr}")
     with conn:
-        print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
 
             message = data.decode().strip()
-            print("Received:", message)
+            print(f"{addr} -> message")
 
             if message.upper().startswith("GET "):
                 key = message[4:].strip()
                 value = store.get(key, "KEY_NOT_FOUND")
                 conn.sendall(value.encode())
-            else:
-                conn.sendall(b"INVALID COMMAND")
