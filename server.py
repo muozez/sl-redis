3e1873dc9475f55548ca6496bd50ce122dc5492c
muozez <muhammetylmaz128@gmail.com>
2026-01-22 16:04:12 +0300

    simple command handler for GET command. I'll implement threading into server.py

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import socket
-import threading
 
 HOST = "127.0.0.1"
 PORT = 1717
@@ -7,11 +6,22 @@ PORT = 1717
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    print("Server listening...")
     conn, addr = s.accept()
+
     with conn:
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+
+            message = data.decode().strip()
+            print("Received:", message)
+
+            if message.upper().startswith("GET "):
+                key = message[4:].strip()
+                value = store.get(key, "KEY_NOT_FOUND")
+                conn.sendall(value.encode())
+            else:
+                conn.sendall(b"INVALID COMMAND")
